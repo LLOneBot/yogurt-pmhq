22cffb114ceead07148c9dcf0bf82c8d80fbe679
Wesley F. Young <wesley.f.young@outlook.com>
2025-11-04 18:51:46 +0800

    update: enable all native targets when run locally

--- buildSrc/src/main/kotlin/kotlin-multiplatform.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-multiplatform.gradle.kts
@@ -7,21 +7,12 @@ plugins {
 kotlin {
     jvm()
 
-    val isBuildingCoreLibrary = System.getenv("GITHUB_ACTIONS_BUILD_CORE_LIBRARY") == "true"
+    // val isBuildingCoreLibrary = System.getenv("GITHUB_ACTIONS_BUILD_CORE_LIBRARY") == "true"
     val actionsTarget = System.getenv("GITHUB_ACTIONS_BUILD_TARGET")
-    val hostOs = System.getProperty("os.name")
-    val arch = System.getProperty("os.arch")
+    // val hostOs = System.getProperty("os.name")
+    // val arch = System.getProperty("os.arch")
     when {
-        // is building core library for all targets
-        isBuildingCoreLibrary -> {
-            mingwX64()
-            macosX64()
-            macosArm64()
-            linuxX64()
-            linuxArm64()
-        }
-
-        // is run from GitHub Actions
+        // is run from GitHub Actions - build Yogurt, improving dependency pulling time
         actionsTarget == "mingwX64" -> mingwX64()
         actionsTarget == "macosX64" -> macosX64()
         actionsTarget == "macosArm64" -> macosArm64()
@@ -29,9 +20,13 @@ kotlin {
         actionsTarget == "linuxArm64" -> linuxArm64()
 
         // is run locally
-        hostOs == "Mac OS X" && arch == "aarch64" -> macosArm64()
-        hostOs == "Linux" && (arch == "x86_64" || arch == "amd64") -> linuxX64()
-        hostOs.startsWith("Windows") -> mingwX64()
+        else -> {
+            mingwX64()
+            macosX64()
+            macosArm64()
+            linuxX64()
+            linuxArm64()
+        }
     }
 
     jvmToolchain(21)
