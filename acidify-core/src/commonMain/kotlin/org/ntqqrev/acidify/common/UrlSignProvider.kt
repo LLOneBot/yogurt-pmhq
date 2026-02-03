d55942b2eb1e74b29f007ef9bcba809e9a277080
Wei FeiYang <wesley.f.young@outlook.com>
2026-02-03 12:00:24 +0800

    refactor(core): use `AppInfo.fromJson` for AppInfo fetching

--- acidify-core/src/commonMain/kotlin/org/ntqqrev/acidify/common/UrlSignProvider.kt
+++ acidify-core/src/commonMain/kotlin/org/ntqqrev/acidify/common/UrlSignProvider.kt
@@ -3,12 +3,10 @@ package org.ntqqrev.acidify.common
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.engine.*
-import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
 
 /**
  * 通过 HTTP 接口进行签名的 [SignProvider] 实现
@@ -17,9 +15,6 @@ import kotlinx.serialization.json.Json
  */
 class UrlSignProvider(val url: String, val httpProxy: String? = null) : SignProvider {
     private val client = HttpClient {
-        install(ContentNegotiation) {
-            json(Json { ignoreUnknownKeys = true })
-        }
         engine {
             if (!httpProxy.isNullOrEmpty()) {
                 proxy = ProxyBuilder.http(httpProxy)
@@ -45,7 +40,7 @@ class UrlSignProvider(val url: String, val httpProxy: String? = null) : SignProv
     suspend fun getAppInfo(): AppInfo? {
         val response = client.get("$url/appinfo")
         return if (response.status == HttpStatusCode.OK) {
-            response.body<AppInfo>()
+            AppInfo.fromJson(response.bodyAsText())
         } else {
             null
         }
